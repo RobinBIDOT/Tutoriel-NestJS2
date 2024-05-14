ede79408e6546da5db213aaf6b5de475d9acfb32
Robin BIDOT <rbidot@hotmail.fr>
2024-05-14 17:59:45 +0200

    delete post

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Post, Req, UseGuards} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Req, UseGuards} from '@nestjs/common';
 import {PostService} from "./post.service";
 import {AuthGuard} from "@nestjs/passport";
 import {CreatePostDto} from "./dto/createPostDto";
@@ -8,10 +8,22 @@ import {Request} from "express";
 export class PostController {
     constructor(private readonly postService : PostService) {}
 
+    @Get()
+    getAll() {
+        return this.postService.getAll()
+    }
+
     @UseGuards(AuthGuard("jwt"))
     @Post("create")
     create(@Body() createPostDto : CreatePostDto, @Req() request : Request) {
         const userId = request.user["userId"]
         return this.postService.create(createPostDto, userId)
+    }
+
+    @UseGuards(AuthGuard("jwt"))
+    @Delete("delete/:id")
+    delete(@Param("id", ParseIntPipe) postId : number, createPostDto : CreatePostDto, @Req() request : Request) {
+        const userId = request.user["userId"]
+        return this.postService.delete(postId, userId)
     }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,13 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import {ForbiddenException, Injectable, NotFoundException} from '@nestjs/common';
 import {PrismaService} from "../prisma/prisma.service";
 import {CreatePostDto} from "./dto/createPostDto";
 
 @Injectable()
 export class PostService {
     constructor(private readonly prismaService : PrismaService) {}
+
+    async getAll() {
+        return this.prismaService.post.findMany({
+            include: {
+                user: {
+                    select: {
+                        username: true,
+                        email: true,
+                        password: false
+                    }
+                },
+                comments: {
+                    include: {
+                        user: {
+                            select: {
+                                username: true,
+                                email: true,
+                                password: false
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+
     async create(createPostDto: CreatePostDto, userId: any) {
         const {body,title} = createPostDto
         await this.prismaService.post.create({data : {body, title, userId}})
         return {data : "Post created"}
+    }
+
+
+    async delete(postId: number, userId: any) {
+        const post = await this.prismaService.post.findUnique({where : {postId}})
+        if (!post) throw new NotFoundException("Post not found")
+        if (post.userId !== userId) throw new ForbiddenException("Forbidden action")
+        await this.prismaService.post.delete({where : {postId}})
+        return {data : "Post deleted"}
     }
 }
